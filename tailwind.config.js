d6983036d38877a73f9348950c8731efc2f17df8
nour fa <nelmauhoub@gmail.com>
2023-02-10 12:46:34 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports={
+    theme:{
+        fontFamily: {
+            'roboto': ['Roboto Mono','monospace']
+          ,
+           'trispace':['Trispace', 'sans-serif'],
+           'poppins': ['Poppins', 'sans-serif'] 
+
+        }
+    }
+}
